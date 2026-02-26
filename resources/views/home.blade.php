0bea1cc04d596a4a5e6fc16c50f00d8e3b9c6bff
Furalmiqbadi <abdulghofuralmiqbadi2@gmail.com>
2026-02-26 07:38:30 +0700

    2

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<html>
+	<body>
+    	<h1>Selamat Datang di Aplikasi POS</h1>
+	</body>
+</html>
---
+++ resources/views/sales.blade.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title>Halaman Transaksi POS</title>
+    </head>
+    <body>
+        <h1>Halaman Transaksi POS</h1>
+        <p>Gunakan halaman ini untuk memproses penjualan produk.</p>
+        <hr>
+        <table border="1">
+            <tr>
+                <th>Item</th>
+                <th>Jumlah</th>
+                <th>Harga</th>
+            </tr>
+            <tr>
+                <td>Pengharum Ruangan</td>
+                <td>1</td>
+                <td>Rp 10.000</td>
+            </tr>
+            <tr>
+                <td>Teh Botol</td>
+                <td>2</td>
+                <td>Rp 8.000</td>
+            </tr>
+            <tr>
+                <td>Popok Bayi</td>
+                <td>1</td>
+                <td>Rp 25.000</td>
+            </tr>
+            <tr>
+                <td colspan="2"><strong>Total</strong></td>
+                <td><strong>Rp 43.000</strong></td>
+            </tr>
+        </table>
+    </body>
+</html>
