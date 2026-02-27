ed1b5fcfb142441b41f06aa4983582c580dbccf3
Furalmiqbadi <abdulghofuralmiqbadi2@gmail.com>
2026-02-28 00:22:48 +0700

    3.2.1

--- database/migrations/2026_02_27_172100_create_m_kategori_table.php
+++ database/migrations/2026_02_27_172100_create_m_kategori_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('m_level', function (Blueprint $table) {
+        Schema::create('m_kategori', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('m_level');
+        Schema::dropIfExists('m_kategori');
     }
 };
--- database/migrations/2026_02_27_172131_create_m_user_table.php
+++ database/migrations/2026_02_27_172131_create_m_user_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('m_level', function (Blueprint $table) {
-            $table->id('level_id');
-            $table->string('level_code', 10)->unique();
-            $table->string('level_name', 100);
+        Schema::create('m_user', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
         });
     }
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('m_level');
+        Schema::dropIfExists('m_user');
     }
 };
