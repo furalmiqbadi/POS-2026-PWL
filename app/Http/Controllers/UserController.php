a20c2ee63816fcc57abb8745104fb056db5d6513
Furalmiqbadi <abdulghofuralmiqbadi2@gmail.com>
2026-03-07 22:02:36 +0700

    4.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,30 +11,28 @@ class UserController extends Controller
 {
     public function index()
     {
-        // // tambah data user dengan Eloquent Model
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data); // tambahkan data ke tabel m_user
-
-        // // coba akses model UserModel
-        // $user = UserModel::all(); // ambil semua data dari tabel m_user
-        // return view('user', ['data' => $user]);
-
-        // 1. Siapkan data yang akan diupdate
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
         ];
+        UserModel::create($data);
 
-        // 2. Update data user berdasarkan username menggunakan Eloquent
-        UserModel::where('username', 'customer-1')->update($data);
-
-        // 3. Ambil semua data terbaru untuk ditampilkan ke view
         $user = UserModel::all();
         return view('user', ['data' => $user]);
+
+        // // 1. Siapkan data yang akan diupdate
+        // $data = [
+        //     'nama' => 'Pelanggan Pertama',
+        // ];
+
+        // // 2. Update data user berdasarkan username menggunakan Eloquent
+        // UserModel::where('username', 'customer-1')->update($data);
+
+        // // 3. Ambil semua data terbaru untuk ditampilkan ke view
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
 
     }
 }
